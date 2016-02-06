22f245f04db17842c9d7afa289be6f50112015fa
amanbis <amanb91@gmail.com>
2016-02-06 18:20:07 +0700

    Test API

--- app/components/main/mainController.js
+++ app/components/main/mainController.js
@@ -3,6 +3,12 @@
 
 	angular
 		.module('app')
+		.config(function($httpProvider){
+		    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+		});
+
+	angular
+		.module('app')
 		.controller('MainController', MainController);
 
 	MainController.$inject = ['$http'];
@@ -12,7 +18,7 @@
 
 		vm.hello = 'hello';
 
-		$http.get('http://www.cheapshark.com/api/1.0/deals?storeID=6&desc=0&title=civilization%20V&pageSize=2')
+		$http.get('http://pokeapi.co/api/v1/pokemon/1/')
 			.then(successCallback, errorCallback);
 
 		function successCallback(response) {
