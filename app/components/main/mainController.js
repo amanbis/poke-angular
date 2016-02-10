5e0e10b65221af4312bcf9d560ff0f28f41cf35a
amanbis <amanb91@gmail.com>
2016-02-10 21:04:12 +0700

    Add query search function

--- app/components/main/mainController.js
+++ app/components/main/mainController.js
@@ -5,32 +5,42 @@
 		.module('app')
 		.controller('MainController', MainController);
 
-	MainController.$inject = ['dataservice'];
+	MainController.$inject = ['dataservice', '$filter'];
 
-	function MainController(dataservice) {
+	function MainController(dataservice, $filter) {
 		var vm = this;
 
+		//Functions of the VM
+		vm.querySearch = querySearch;
 		vm.searchTextChange = searchTextChange;
 		vm.selectedItemChange = selectedItemChange;
 
 		vm.searchText = '';
 		vm.selectedItem = '';
-		vm.pokemon = [];
+		vm.pokedex = [];
 
 		activate();
 
 		function activate() {
 			return getPokemon().then(function() {
-				console.log(vm.pokemon);
+				console.log(vm.pokedex);
 			});
 		}
 
 		function getPokemon() {
 			return dataservice.getPokemon()
 				.then(function(data) {
-					vm.pokemon.push.apply(vm.pokemon, data);
-					return vm.pokemon;
+					vm.pokedex.push.apply(vm.pokedex, sortPokemon(data));
+					return vm.pokedex;
 				});
+		}
+
+		function sortPokemon(data) {
+			return $filter('orderBy')(data, 'name');
+		}
+
+		function querySearch(query) {
+			return query ? $filter('filter')(vm.pokedex, query) : vm.pokedex;
 		}
 
 		function searchTextChange(text) {
@@ -38,7 +48,7 @@
 		}
 
 		function selectedItemChange(item) {
-			console.log('Item changed to ' + item.name);
+			console.log('Item changed to ' + (item ? item.name + 'url: ' + item.resource_uri : item) );
 		}
 	}
 
